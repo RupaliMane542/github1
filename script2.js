203ec23218e182451a3d3c5b11e71352339060e8
rupalimane542 <rupalimane542@gmail.com>
2023-08-26 22:09:38 +0530

    added

--- script2.js
+++ script2.js
@@ -1,2 +1,7 @@
 console.log("I am learning github")
 console.log("repo creation sucessful")
+console.log("repo creation sucessful")
+console.log("repo creation sucessful")
+console.log("repo creation sucessful")
+
+console.log("repo creation sucessful")
